d51ba98252ef1b5515456fee23fbb7f40512f914
章渝昆 <zhangyk@allinpay.com>
2020-09-11 13:48:56 +0800

    更新nacos配置中心

--- provider/src/main/java/com/allinpay/provider/ProviderApplication.java
+++ provider/src/main/java/com/allinpay/provider/ProviderApplication.java
@@ -2,16 +2,11 @@ package com.allinpay.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProviderApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext= SpringApplication.run(ProviderApplication.class,args);
-        String userName=configurableApplicationContext.getEnvironment().getProperty("user.name");
-        String userAge=configurableApplicationContext.getEnvironment().getProperty("user.age");
-        System.out.println("userName: "+userName);
-        System.out.println("userAge: "+userAge);
+        SpringApplication.run(ProviderApplication.class,args);
     }
 }
