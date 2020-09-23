f7190f900893c72307c9fd5d1f41f4eef6fb0f4c
章渝昆 <zhangyk@allinpay.com>
2020-09-23 17:17:20 +0800

    bugfix

--- zuul-gateway/src/main/java/com/allinpay/zuul/filter/SimpleFilter.java
+++ zuul-gateway/src/main/java/com/allinpay/zuul/filter/SimpleFilter.java
@@ -1,38 +1,40 @@
 package com.allinpay.zuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.constants.ZuulConstants;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class SimpleFilter extends ZuulFilter {
 
-    private static Logger logger= LoggerFactory.getLogger(SimpleFilter.class);
+    private static final Logger logger= LoggerFactory.getLogger(SimpleFilter.class);
 
     @Override
     public String filterType() {
-        return "pre";
+        return FilterConstants.PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return 0;
+        return 1;
     }
 
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
 
     @Override
-    public Object run() throws ZuulException {
+    public Object run()  {
         RequestContext ctx=RequestContext.getCurrentContext();
         HttpServletRequest request =ctx.getRequest();
-        logger.info("{} request to {}",request.getMethod(),request.getRequestURI().toString());
+        logger.info("ctx : {}",ctx);
 
-        return null;
+        return true;
     }
 }
